725491873633c8eaa2a1d421434d38a1d12ec490
Axel Jalonen <axeljalonen@gmail.com>
2024-12-15 20:38:26 -0800

    Update meta

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Access-Control-Allow-Origin": "http://localhost:5173", // Restrict to your dev server's origin
+    },
+  },
   build: {
     outDir: "./dist",
     rollupOptions: {
